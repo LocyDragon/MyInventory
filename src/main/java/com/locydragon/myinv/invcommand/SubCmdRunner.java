a0e35052169ac4a29b16590320705884cb0c8cff
LocyDragon <2424441676@qq.com>
2018-09-08 22:09:33 +0800

    fix a maven bug!

--- src/main/java/com/locydragon/myinv/invcommand/SubCmdRunner.java
+++ src/main/java/com/locydragon/myinv/invcommand/SubCmdRunner.java
@@ -1,13 +1,12 @@
 package com.locydragon.myinv.invcommand;
 
 /**
- * @apiNote 指令执行者
  * @author LocyDragon
  */
 public interface SubCmdRunner {
 	/**
 	 * 当被执行时调用
-	 * @param info
+	 * @param info 传入的参数
 	 */
 	void onSubCommand(SubCommandInfo info);
 }
